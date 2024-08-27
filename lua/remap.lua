ab4120795f294049076753a58b026af73bff949b
Kelan Mazey <kelanmazey@gmail.com>
2024-08-27 08:25:31 +0100

    nvim config

---
+++ lua/remap.lua
@@ -0,0 +1,21 @@
+vim.g.mapleader = " "
+
+-- telescope
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>f', builtin.find_files, {})
+vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>ps', function()
+	builtin.grep_string({ search = vim.fn.input("Grep > ") });
+end)
+
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+
+-- harpoon
+local harpoon = require("harpoon")
+
+harpoon:setup()
+vim.keymap.set("n", "<leader>p", function() harpoon:list():add() end)
+vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
